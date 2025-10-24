30ca970460412af6f3bd3d73bf8f08cd2f56f90e
Klaus Zanders <klaustopher@users.noreply.github.com>
2025-10-24 11:55:16 +0200

    Update modules/meeting/db/migrate/20251022114752_add_participation_status_for_meeting_participant.rb
    Co-authored-by: Oliver Günther <o.guenther@openproject.com>

--- modules/meeting/db/migrate/20251022114752_add_participation_status_for_meeting_participant.rb
+++ modules/meeting/db/migrate/20251022114752_add_participation_status_for_meeting_participant.rb
@@ -3,11 +3,15 @@
 class AddParticipationStatusForMeetingParticipant < ActiveRecord::Migration[8.0]
   def change
     add_column :meeting_participants, :participation_status, :string, null: true
-    execute <<-SQL.squish
-      UPDATE meeting_participants
-      SET participation_status = 'unknown'
-      WHERE participation_status IS NULL
-    SQL
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL.squish
+          UPDATE meeting_participants
+          SET participation_status = 'unknown'
+          WHERE participation_status IS NULL
+        SQL
+      end
+    end    
     change_column_default :meeting_participants, :participation_status, "needs-action"
     change_column_null :meeting_participants, :participation_status, false
   end
