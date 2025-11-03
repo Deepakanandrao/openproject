2f42a60ad0456e54e22eaec9f4b7c695430aab7e
Kabiru Mwenja <k.mwenja@openproject.com>
2025-11-03 21:56:16 +0300

    Extract `Paginator::JournalChangesFilter` for better separation of concerns

--- app/services/work_packages/activities_tab/paginator.rb
+++ app/services/work_packages/activities_tab/paginator.rb
@@ -140,10 +140,12 @@ class WorkPackages::ActivitiesTab::Paginator
       .reorder(version: :desc) # Always fetch newest first for pagination
       .with_sequence_version
 
-    journals = journals.where.not(notes: [nil, ""]) if filter == :only_comments
-    journals = apply_only_changes_filter_heuristic(journals) if filter == :only_changes
-
-    journals
+    case filter
+    when :only_comments then apply_comments_only_filter(journals)
+    when :only_changes then apply_changes_only_filter(journals)
+    else
+      journals
+    end
   end
 
   def fetch_revisions
@@ -167,137 +169,11 @@ class WorkPackages::ActivitiesTab::Paginator
     end
   end
 
-  # SQL-based heuristic to filter journals with changes.
-  #
-  # Includes journals that have:
-  #   * Initial journal (version = 1) - always included
-  #   * Attachment changes (compares attachable_journals with predecessor)
-  #   * Custom field changes (compares customizable_journals with predecessor)
-  #   * File link changes (compares storages_file_links_journals with predecessor)
-  #   * Cause metadata (system-triggered changes)
-  #   * Attribute/data changes (compares work_package_journals columns with immediate predecessor)
-  #
-  # This heuristic compares association records with the predecessor journal to detect actual changes,
-  # not just the presence of snapshot records.
-  def apply_only_changes_filter_heuristic(journals)
-    sql = <<~SQL.squish
-      version = 1
-      OR (cause IS NOT NULL AND cause != '{}')
-      OR EXISTS (#{attribute_data_changes_condition_sql})
-      OR EXISTS (#{attachment_changes_condition_sql})
-      OR EXISTS (#{custom_field_changes_condition_sql})
-      OR EXISTS (#{file_link_changes_condition_sql})
-    SQL
-
-    journals.where(OpenProject::SqlSanitization.sanitize(sql))
+  def apply_comments_only_filter(scope)
+    scope.where.not(notes: [nil, ""])
   end
 
-  def attribute_data_changes_condition_sql
-    <<~SQL.squish
-      SELECT 1
-        FROM journals predecessor
-        INNER JOIN work_package_journals pred_data ON predecessor.data_id = pred_data.id
-        INNER JOIN work_package_journals curr_data ON journals.data_id = curr_data.id
-        WHERE predecessor.journable_id = journals.journable_id
-          AND predecessor.journable_type = journals.journable_type
-          AND predecessor.version = (#{max_predecessor_version_sql})
-          AND (#{data_changes_condition_sql})
-    SQL
-  end
-
-  # Identify the immediate predecessor journal for comparison.
-  # NB: Journal versions are incremental but not guaranteed to be sequential.
-  def max_predecessor_version_sql
-    <<~SQL.squish
-      SELECT MAX(version)
-      FROM journals p2
-      WHERE p2.journable_id = journals.journable_id
-        AND p2.journable_type = journals.journable_type
-        AND p2.version < journals.version
-    SQL
-  end
-
-  # Detect changes in work_package_journals columns.
-  def data_changes_condition_sql
-    data_change_columns.map do |column_name|
-      "pred_data.#{column_name} IS DISTINCT FROM curr_data.#{column_name}"
-    end.join(" OR ")
-  end
-
-  def data_change_columns
-    Journal::WorkPackageJournal.column_names - ["id", "project_phase_definition_id"]
-  end
-
-  def attachment_changes_condition_sql
-    association_changes_condition_sql(
-      table: Journal::AttachableJournal.table_name,
-      id_column: "attachment_id",
-      value_columns: ["filename"]
-    )
-  end
-
-  def custom_field_changes_condition_sql
-    association_changes_condition_sql(
-      table: Journal::CustomizableJournal.table_name,
-      id_column: "custom_field_id",
-      value_columns: ["value"]
-    )
-  end
-
-  def file_link_changes_condition_sql
-    association_changes_condition_sql(
-      table: Journal::StorableJournal.table_name,
-      id_column: "file_link_id",
-      value_columns: %w[link_name storage_name]
-    )
-  end
-
-  # Detect changes in association journals by checking for additions and removals.
-  def association_changes_condition_sql(table:, id_column:, value_columns:)
-    "#{association_items_added_sql(table:, id_column:, value_columns:)} " \
-      "UNION " \
-      "#{association_items_removed_sql(table:, id_column:)}"
-  end
-
-  # Detect added or modified association items by comparing with predecessor journal.
-  # Returns SQL that finds items that either:
-  # - Exist in current journal but not in predecessor (additions)
-  # - Exist in both but have different values (modifications)
-  def association_items_added_sql(table:, id_column:, value_columns:)
-    value_changes = value_columns.map do |col|
-      "pred.#{col} IS DISTINCT FROM curr.#{col}"
-    end.join(" OR ")
-
-    <<~SQL.squish
-      SELECT 1
-        FROM #{table} curr
-        LEFT JOIN journals predecessor
-          ON predecessor.journable_id = journals.journable_id
-          AND predecessor.journable_type = journals.journable_type
-          AND predecessor.version = (#{max_predecessor_version_sql})
-        LEFT JOIN #{table} pred
-          ON pred.journal_id = predecessor.id
-          AND pred.#{id_column} = curr.#{id_column}
-        WHERE curr.journal_id = journals.id
-          AND (pred.id IS NULL OR (#{value_changes}))
-    SQL
-  end
-
-  # Detect removed association items by comparing with predecessor journal.
-  # Returns SQL that finds items that existed in predecessor but not in current journal.
-  def association_items_removed_sql(table:, id_column:)
-    <<~SQL.squish
-      SELECT 1
-        FROM journals predecessor
-        INNER JOIN #{table} pred
-          ON pred.journal_id = predecessor.id
-        LEFT JOIN #{table} curr
-          ON curr.journal_id = journals.id
-          AND curr.#{id_column} = pred.#{id_column}
-        WHERE predecessor.journable_id = journals.journable_id
-          AND predecessor.journable_type = journals.journable_type
-          AND predecessor.version = (#{max_predecessor_version_sql})
-          AND curr.id IS NULL
-    SQL
+  def apply_changes_only_filter(scope)
+    JournalChangesFilter.apply(scope)
   end
 end
