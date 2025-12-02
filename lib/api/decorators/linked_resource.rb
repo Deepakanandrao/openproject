darkdragon18thuniverse/clickdown

Avoid deprecation warnings for time and cost entries representers
They both use `work_package` and `work_package_id` to render links,
which generates verbose deprecation warnings. They are filling up logs.

Force usage of `entity` and `entity_id` instead in the api representers
to reduce the burden.

lib/api/decorators/linked_resource.rb
@@ -90,7 +90,6 @@ module API
                      show_if: ->(*) { true },
                      skip_render: nil,
                      embedded: true)
-
           link(link_attr(name, uncacheable_link, link_cache_if), &link)
 
           property name,
@@ -113,7 +112,6 @@ module API
                       show_if: ->(*) { true },
                       skip_render: nil,
                       embedded: true)
-
           links(link_attr(name, uncacheable_link, link_cache_if), &link)
 
           property name,
@@ -131,7 +129,6 @@ module API
                           setter:,
                           getter:,
                           show_if: ->(*) { true })
-
           resource(name,
                    getter: ->(*) {},
                    setter:,
@@ -154,16 +151,18 @@ module API
                                 skip_link: skip_render,
                                 undisclosed: false,
                                 link_title_attribute: :name,
+                                link_getter: :"#{name}_id",
+                                link_property_name: nil,
                                 uncacheable_link: false,
                                 getter: associated_resource_default_getter(name, representer),
                                 setter: associated_resource_default_setter(name, as, v3_path),
-                                link: associated_resource_default_link(name,
+                                link: associated_resource_default_link(link_property_name || name,
                                                                        v3_path:,
                                                                        skip_link:,
                                                                        undisclosed:,
-                                                                       title_attribute: link_title_attribute))
-
-          resource((as || name),
+                                                                       title_attribute: link_title_attribute,
+                                                                       getter: link_getter))
+          resource(as || name,
                    getter:,
                    setter:,
                    link:,
@@ -240,7 +239,6 @@ module API
                                                                          v3_path:,
                                                                          skip_link:,
                                                                          title_attribute: link_title_attribute))
-
           resources(as,
                     getter:,
                     setter:,
@@ -251,7 +249,6 @@ module API
 
         def associated_resources_default_getter(name,
                                                 representer)
-
           representer ||= default_representer(name.to_s.singularize)
 
           ->(*) do

Deepakanandrao/openproject

lib/api/decorators/linked_resource.rb
@@ -174,14 +174,17 @@ module API
                                 skip_link: skip_render,
                                 undisclosed: false,
                                 link_title_attribute: :name,
+                                link_getter: :"#{name}_id",
+                                link_property_name: nil,
                                 uncacheable_link: false,
                                 getter: associated_resource_default_getter(name, representer),
                                 setter: associated_resource_default_setter(name, as, v3_path),
-                                link: associated_resource_default_link_lambda(name,
+                                link: associated_resource_default_link_lambda(link_property_name || name,
                                                                               v3_path:,
                                                                               skip_link:,
                                                                               undisclosed:,
-                                                                              title_attribute: link_title_attribute))
+                                                                              title_attribute: link_title_attribute,
+                                                                              getter: link_getter))
           resource(as || name,
                    getter:,
                    setter:,