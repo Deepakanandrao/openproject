8a6dcb414884d73a586139eb63d3154f1e800199
Eric Schubert <e.schubert@openproject.com>
2025-12-12 14:53:59 +0100

    [#69906] deferred loading of item actions
    - https://community.openproject.org/work_packages/69906
    - load item actions only when clicking on three dots menu
    - drastically reduce response size when initially load all items of a
      single level

--- config/routes.rb
+++ config/routes.rb
@@ -222,6 +222,7 @@ Rails.application.routes.draw do
             get :change_parent, action: :change_parent_dialog
             post :change_parent, action: :change_parent
             get :delete, action: :deletion_dialog
+            get :item_actions
             post :move
             get :new_child, action: :new
             post :new_child, action: :create
@@ -683,6 +684,7 @@ Rails.application.routes.draw do
             get :change_parent, action: :change_parent_dialog
             post :change_parent, action: :change_parent
             get :delete, action: :deletion_dialog
+            get :item_actions
             post :move
             get :new_child, action: :new
             post :new_child, action: :create
