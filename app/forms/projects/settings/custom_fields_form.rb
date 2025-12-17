20267baea40a16d62fa7282187540ea6f8f52599
ulferts <jens.ulferts@googlemail.com>
2025-12-17 18:07:45 +0100

    inline sql statement into another one to avoid one request

--- app/forms/projects/settings/custom_fields_form.rb
+++ app/forms/projects/settings/custom_fields_form.rb
@@ -43,12 +43,14 @@ module Projects
       private
 
       def custom_fields
-        enabled_custom_fields = model.enabled_custom_field_ids.presence || ProjectCustomField.for_all.pluck(:id)
+        @custom_fields ||= begin
+          enabled_custom_fields = model.enabled_custom_field_ids.presence || ProjectCustomField.for_all.select(:id)
 
-        @custom_fields ||= model
-          .available_custom_fields
-          .where(id: enabled_custom_fields)
-          .required
+          model
+            .available_custom_fields
+            .where(id: enabled_custom_fields)
+            .required
+        end
       end
     end
   end
