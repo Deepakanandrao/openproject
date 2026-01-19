0aff9ebd62434627dce3016cd7473937b15a5f57
Bruno Pagno <b.pagno@openproject.com>
2026-01-19 15:07:48 +0100

    simplify packaging collaborative sign in parameters

--- modules/documents/app/controllers/documents_controller.rb
+++ modules/documents/app/controllers/documents_controller.rb
@@ -222,35 +222,30 @@ class DocumentsController < ApplicationController
     redirect_to document_path(call.result, state: :edit)
   end
 
-  # rubocop:disable Metrics/AbcSize
   def define_token_payload
-    oauth_token = generate_encrypted_oauth_token
+    oauth_token = generate_oauth_token
 
     @resource_url = URI.join(
       root_url,
       API::V3::Utilities::PathHelper::ApiV3Path.document(@document.id)
     ).to_s
 
-    secret = Documents::OAuth::DigestSecretService
-      .new(Setting.collaborative_editing_hocuspocus_secret)
-      .call
-    return Rails.logger.error(secret.errors) if secret.failure?
+    payload = {
+      resource_url: @resource_url,
+      oauth_token:,
+      readonly: @readonly
+    }
 
-    @token_payload = JWT.encode(
-      {
-        resource_url: @resource_url,
-        oauth_token:,
-        readonly: @readonly,
-        exp: 2.minutes.from_now.to_i
-      },
-      secret.result,
-      "HS256"
-    )
+    encrypted_payload = Documents::OAuth::EncryptTokenService.new(token: payload.to_json).call
+    if encrypted_payload.failure?
+      Rails.logger.error("Failed to encrypt OAuth token payload for document #{@document.id}: #{encrypted_payload.errors}")
+      return
+    end
+
+    @token_payload = encrypted_payload.result
   end
-  # rubocop:enable Metrics/AbcSize
 
-  # rubocop:disable Metrics/AbcSize
-  def generate_encrypted_oauth_token
+  def generate_oauth_token
     if !current_user.allowed_in_project?(:view_documents, @project)
       return
     end
@@ -264,18 +259,8 @@ class DocumentsController < ApplicationController
       return
     end
 
-    result = Documents::OAuth::EncryptTokenService
-      .new(token: result.result.plaintext_token)
-      .call
-
-    if result.failure?
-      Rails.logger.error("Failed to encrypt OAuth token for document #{@document.id}: #{result.errors}")
-      return
-    end
-
-    result.result
+    result.result.plaintext_token
   end
-  # rubocop:enable Metrics/AbcSize
 
   def update_header_component_via_turbo_stream(state: :show)
     update_via_turbo_stream(
