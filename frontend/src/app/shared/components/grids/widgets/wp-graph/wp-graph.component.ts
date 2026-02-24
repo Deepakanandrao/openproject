211c9737d14e92fd03ce8f1a1a2767ef0a78bf88
Henriette Darge <h.darge@openproject.com>
2026-02-24 10:32:55 +0100

    Apply widget menu label to all widget menus

--- frontend/src/app/shared/components/grids/widgets/wp-graph/wp-graph.component.ts
+++ frontend/src/app/shared/components/grids/widgets/wp-graph/wp-graph.component.ts
@@ -85,8 +85,4 @@ export class WidgetWpGraphComponent extends AbstractWidgetComponent implements O
   public get chartType() {
     return this.graphConfiguration.chartType;
   }
-
-  public get widgetMenuLabel():string {
-    return this.i18n.t('js.grid.widget_menu_label', { widgetName: this.widgetName });
-  }
 }
